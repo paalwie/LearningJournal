69df443e119abb9043fdbdd229a42b1b6a11d8a4
Patrick Albert Wiedenmann <34657607+paalwie@users.noreply.github.com>
2024-09-18 08:31:11 +0200

    Add files via upload

---
+++ src/main/java/com/Journal/LearningJournal/LearningJournalApplication.java
@@ -0,0 +1,13 @@
+package com.Journal.LearningJournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Journal/LearningJournal/LearningJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Journal.LearningJournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
